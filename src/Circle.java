7e0a73e118adedca968d9caeed57da4998c0b16f
Yera17 <erko.salaly@gmail.com>
2024-01-18 12:48:32 +0100

    Task2

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,12 @@
-public class Circle {
+public class Circle implements Shape {
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return 3.14*(radius*radius);
+    }
 }
--- src/Parallelogram.java
+++ src/Parallelogram.java
@@ -1,2 +1,14 @@
-public class Parallelogram {
+public class Parallelogram implements Shape {
+    private final double a;
+    private final double b;
+
+    public Parallelogram(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public double getArea() {
+        return a*b;
+    }
 }
